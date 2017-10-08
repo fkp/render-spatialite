99acd3dfdfe5e038d08f13ef6d476e04b77314e9
fkp <no@spam.com>
2017-10-08 21:16:49 +0100

    Something quick working.

---
+++ client/TestApp/TestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETMapnik;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Map m = new Map(256, 256);
+                Image i = new Image(256, 256);
+                Mapnik.RegisterDatasource(System.IO.Path.Combine(Mapnik.Paths["InputPlugins"], "shape..input"));
+                m.Load(@"..\..\test.xml");
+                m.ZoomAll();
+                m.Render(i);
+                i.Save("output.png");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
