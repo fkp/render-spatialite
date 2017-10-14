3fdfb57f83e73c68695894436428e32896e2a4c2
fkp <no@spam.com>
2017-10-14 14:36:46 +0100

    first cut of rendering prototypes

---
+++ client/TestAppUI/TestAppUI/TestAppUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using NETMapnik;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestAppUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Mapnik.RegisterDatasource(System.IO.Path.Combine(Mapnik.Paths["InputPlugins"], "sqlite..input"));
+
+            Map m = new Map(256, 256);
+            m.Load(@"..\..\test.xml");
+            m.ZoomAll();
+
+            var i = new NETMapnik.Image(256, 256);
+            var iv = i.View(0, 0, 256, 256);
+            m.Render(i);
+
+            var decoder = new PngBitmapDecoder(new MemoryStream(iv.Encode("png")), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            BitmapSource bitmapSource = decoder.Frames[0];
+
+            mapImage.Source = bitmapSource;
+        }
+    }
+}
