b90dc28fd61e5f48513ba23a525446bf021e5e2f
fkp <no@spam.com>
2017-10-23 21:35:21 +0100

    Params tweak

--- scripts/buildSqlite.sh
+++ scripts/buildSqlite.sh
@@ -1,4 +1,4 @@
 srid=$1
 code=EPSG:$srid
 echo building to $code
-ogr2ogr -f SQLite data_$srid.sqlite -t_srs $code PG:"dbname=gis"
+ogr2ogr -f SQLite data_$srid.sqlite -t_srs $code PG:"dbname=gis" -dsco SPATIALITE=YES
