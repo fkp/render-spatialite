0de72f4adb662f6fc1a4015e0daa92f54c3fb528
fkp <no@spam.com>
2017-10-24 22:14:27 +0100

    First cut of working-ish stylesheet.

---
+++ client/TestAppUI/TestAppUI/TestAppUI/Location.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppUI
+{
+    class Location
+    {
+        double _centreX;
+        double _centreY;
+        double _width;
+        double _height;
+
+        public Location(double centreX, double centreY, double width, double height)
+        {
+            _centreX = centreX;
+            _centreY = centreY;
+            _width = width;
+            _height = height;
+        }
+
+        public double CentreX { get { return _centreX; } }
+        public double CentreY { get { return _centreY; } }
+        public double Width { get { return _width; } }
+        public double Height { get { return _height; } }
+
+        public void ZoomIn(double factor = 2)
+        {
+            _width /= factor;
+            _height /= factor;
+        }
+
+        public void ZoomOut(double factor = 2)
+        {
+            _width *= factor;
+            _height *= factor;
+        }
+
+        public void MoveX(double increment)
+        {
+            _centreX += increment;
+        }
+
+        public void MoveY(double increment)
+        {
+            _centreY += increment;
+        }
+
+        public double MinX { get { return _centreX - (_width / 2); } }
+        public double MinY { get { return _centreY - (_height / 2); } }
+        public double MaxX { get { return _centreX + (_width / 2); } }
+        public double MaxY { get { return _centreY + (_height / 2); } }
+    }
+}
--- client/TestAppUI/TestAppUI/TestAppUI/MainWindow.xaml.cs
+++ client/TestAppUI/TestAppUI/TestAppUI/MainWindow.xaml.cs
@@ -22,24 +22,74 @@ namespace TestAppUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        Map _m;
+        Location _location;
+
         public MainWindow()
         {
             InitializeComponent();
 
             Mapnik.RegisterDatasource(System.IO.Path.Combine(Mapnik.Paths["InputPlugins"], "sqlite..input"));
+            Mapnik.RegisterDatasource(System.IO.Path.Combine(Mapnik.Paths["InputPlugins"], "shape..input"));
+            Mapnik.RegisterSystemFonts();
 
-            Map m = new Map(256, 256);
-            m.Load(@"..\..\test.xml");
-            m.ZoomAll();
+            _m = new Map(700, 500);
+            _location = new Location(325187, 673924, 500, 500);
+            _m.Load(@"..\..\osm_27700_slim.xml");
 
-            var i = new NETMapnik.Image(256, 256);
-            var iv = i.View(0, 0, 256, 256);
-            m.Render(i);
+            Render();
+        }
 
-            var decoder = new PngBitmapDecoder(new MemoryStream(iv.Encode("png")), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            BitmapSource bitmapSource = decoder.Frames[0];
+        private void Render()
+        {
+            //m.ZoomAll();
+            _m.ZoomToBox(_location.MinX, _location.MinY, _location.MaxX, _location.MaxY);
 
-            mapImage.Source = bitmapSource;
+            using (var i = new NETMapnik.Image(700, 500))
+            using (var iv = i.View(0, 0, 700, 500))
+            {
+                _m.Render(i);
+
+                var decoder = new PngBitmapDecoder(new MemoryStream(iv.Encode("png")), BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                BitmapSource bitmapSource = decoder.Frames[0];
+                mapImage.Source = bitmapSource;
+            }
+        }
+
+        private void Button_Click_Out(object sender, RoutedEventArgs e)
+        {
+            _location.ZoomOut();
+            Render();
+        }
+
+        private void Button_Click_In(object sender, RoutedEventArgs e)
+        {
+            _location.ZoomIn();
+            Render();
+        }
+
+        private void Button_Click_Left(object sender, RoutedEventArgs e)
+        {
+            _location.MoveX(-_location.Width / 2);
+            Render();
+        }
+
+        private void Button_Click_Right(object sender, RoutedEventArgs e)
+        {
+            _location.MoveX(_location.Width / 2);
+            Render();
+        }
+
+        private void Button_Click_Up(object sender, RoutedEventArgs e)
+        {
+            _location.MoveY(_location.Height / 2);
+            Render();
+        }
+
+        private void Button_Click_Down(object sender, RoutedEventArgs e)
+        {
+            _location.MoveY(-_location.Height / 2);
+            Render();
         }
     }
 }
